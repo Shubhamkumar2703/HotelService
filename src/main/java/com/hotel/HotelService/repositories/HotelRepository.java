f2ddce44f213362ab998bf33a22d97a8df85ae8a
shubham kumar <shubham27034@gmail.com>
2026-03-04 15:44:05 +0530

    built hotel service

---
+++ src/main/java/com/hotel/HotelService/repositories/HotelRepository.java
@@ -0,0 +1,9 @@
+package com.hotel.HotelService.repositories;
+
+import com.hotel.HotelService.entites.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRepository extends JpaRepository<Hotel, String>
+{
+
+}
---
+++ src/main/java/com/hotel/HotelService/services/HotelService.java
@@ -0,0 +1,18 @@
+package com.hotel.HotelService.services;
+
+import com.hotel.HotelService.entites.Hotel;
+
+import java.util.List;
+
+public interface HotelService {
+
+    //create
+
+    Hotel create(Hotel hotel);
+
+    //get all
+    List<Hotel> getAll();
+
+    //get single
+    Hotel get(String id);
+}
